4a9bf0a592e797ca1c8483e9a5663e6a65d7a758
mangocodex <bodhi.comparan@gmail.com>
2025-01-21 12:14:47 -0600

    second commit / py files / finished

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-my_money_machine = MoneyMachine()
-my_coffee_maker = CoffeeMaker()
-my_item = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 is_on = True
 while is_on:
-    order = input("What would you like? (espresso/latte/cappuccino/): ").lower()
+    order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    drink = menu.find_drink(order)
     if order == "off":
         print("Powering down.")
         is_on = False
     elif order == "report":
-        my_coffee_maker.report()
-        my_money_machine.report()
-    elif order:
-        my_coffee_maker.is_resource_sufficient(drink=order)
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
